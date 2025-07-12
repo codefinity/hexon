2121064a138ee675fa1a0b1ab5ea6a8eafbd6fba
Nikhil P <nikhilpat@gmail.com>
2025-07-12 15:59:11 +0530

    Created basic Hexagon meta structure attributes.

---
+++ src/hexon/Codefinity.Hexon.Architecture/ComponentTypeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codefinity.Hexon.Architecture
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class ComponentTypeAttribute : Attribute
+    {
+        public string Id { get; }
+
+        public ComponentTypeAttribute(string id)
+        {
+            Id = id;
+        }
+    }
+}
